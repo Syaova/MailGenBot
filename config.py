61537d6d71975f7d5a3b71b3b71dfb87989e6f95
Syaova <147381320+Syaova@users.noreply.github.com>
2026-03-08 11:14:17 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = "8688551630:AAFVMjosCCJmsHmli-daMcSjlPzUr8kO7aM"  # Вставь сюда токен от BotFather
+FREE_LIMIT = 5            # Бесплатный лимит генераций
+LIMIT_RESET_HOURS = 24    # Время восстановления лимита (часы)
+DB = "users.db"           # Файл базы данных
